f8b615b8ab493e7165b277bfb1f145bade62716a
ifejayeola <ijjayeola@gmail.com>
2026-01-19 14:12:42 +0100

    added auth

--- results/permissions.py
+++ results/permissions.py
@@ -2,4 +2,9 @@ from rest_framework import permissions
 
 
 class is_principal(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        
+        return request.user.is_teacher or request.user.is_principal
     
--- results/views.py
+++ results/views.py
@@ -2,50 +2,55 @@ from django.shortcuts import render # type: ignore
 from .models import Student, MyUser, Assessment, TermReport, AcademicSession, Subject, ClassRoom 
 from .serializers import StudentSerializers, UserSerializer, TermReportSerializer, AssessmentSerializer, AcademicSessionSerializer, SubjectSerializer, ClassRoomSerializer
 from rest_framework import viewsets # type: ignore
-from rest_framework.permissions import IsAuthenticated # type: ignore
+from rest_framework.permissions import IsAuthenticated, is_principal # type: ignore
 
 # Create your views here.
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all() #type: ignore
     serializer_class = StudentSerializers
     http_method_names = ['get', 'post', 'patch', 'delete']
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     
     
 class UserViewSet(viewsets.ModelViewSet):
-        queryset = MyUser.objects.all()  #type: ignore
-        http_method_names = ['get', 'post', 'patch', 'delete']
-        serializer_class = UserSerializer
-        # filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    queryset = MyUser.objects.all()  #type: ignore
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
  
 
 class AssessmentViewSet(viewsets.ModelViewSet):
-        queryset = Assessment.objects.all()  #type: ignore
-        http_method_names = ['get', 'post', 'patch', 'delete']
-        serializer_class = AssessmentSerializer
+    queryset = Assessment.objects.all()  #type: ignore
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    serializer_class = AssessmentSerializer
+    permission_classes = [IsAuthenticated]
  
 
 class TermReportViewSet(viewsets.ModelViewSet):
-        queryset = TermReport.objects.all()  #type: ignore
-        http_method_names = ['get', 'patch', 'delete']
-        serializer_class = TermReportSerializer
+    queryset = TermReport.objects.all()  #type: ignore
+    http_method_names = ['get', 'patch', 'delete']
+    serializer_class = TermReportSerializer
+    permission_classes = [IsAuthenticated]
  
 
 class AcademicSessionViewSet(viewsets.ModelViewSet):
-        queryset = AcademicSession.objects.all()  #type: ignore
-        http_method_names = ['get', 'post']
-        serializer_class = AcademicSessionSerializer
+    queryset = AcademicSession.objects.all()  #type: ignore
+    http_method_names = ['get', 'post']
+    serializer_class = AcademicSessionSerializer
+    permission_classes = [IsAuthenticated]
  
 
 class ClassRoomViewSet(viewsets.ModelViewSet):
-        queryset = ClassRoom.objects.all()  #type: ignore
-        http_method_names = ['get', 'post', 'patch', 'delete']
-        serializer_class = ClassRoomSerializer
+    queryset = ClassRoom.objects.all()  #type: ignore
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    serializer_class = ClassRoomSerializer
+    permission_classes = [IsAuthenticated]
  
 
 class SubjectViewSet(viewsets.ModelViewSet):
-        queryset = Subject.objects.all()  #type: ignore
-        http_method_names = ['get', 'post', 'patch', 'delete']
-        serializer_class = SubjectSerializer
+    queryset = Subject.objects.all()  #type: ignore
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    serializer_class = SubjectSerializer
+    permission_classes = [IsAuthenticated]
  
 
